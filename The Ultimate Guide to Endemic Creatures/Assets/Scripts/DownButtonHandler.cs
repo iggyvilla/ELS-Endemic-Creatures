19aeeac07454ae691c588400a59b5212b11e1db5
iggyvilla <iggyvilla@icloud.com>
2021-04-07 22:37:39 +0800

    Added TalkManager

---
+++ The Ultimate Guide to Endemic Creatures/Assets/Scripts/DownButtonHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownButtonHandler : MonoBehaviour
+{
+    public delegate void NextLineDelegate();
+    public event NextLineDelegate nextLineEvent;
+    public AnimationCurve myCurve;
+    bool move = true;
+
+    void Start()
+    {
+        GetComponentInParent<TalkManager>().endConvoEvent += ConvoEnd;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // the bobbing thing
+        if (move) {
+            transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)) - 0.6f, transform.position.z);
+        }
+    }
+
+    void OnMouseDown() {
+        Debug.Log("Down button pressed");
+        nextLineEvent();
+        transform.localScale -= new Vector3(0.05f, 0.05f, 0);
+    }
+
+    void OnMouseUp()
+    {
+        transform.localScale += new Vector3(0.05f, 0.05f, 0);
+    }
+
+    void ConvoEnd() {
+        move = false;
+    }
+}
---
+++ The Ultimate Guide to Endemic Creatures/Assets/Scripts/TalkManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text conversation;
+    public float TextShowDelay;
+    bool nextEvent = false;
+    public delegate void EndConversation();
+    public event EndConversation endConvoEvent;
+
+
+    void Start()
+    {
+        GoOutOfFrame();
+        conversation = GetComponentInChildren<Text>();
+        StartCoroutine(MakeConversation(new string[] {"All your base are belong to us!", "Yahahahaha!"}));
+
+        GetComponentInChildren<DownButtonHandler>().nextLineEvent += GoNextLine; // subscribing to event
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MakeConversation(string[] sentences) {
+        GoInFrame();
+        enabled = true;
+        foreach (string sentence in sentences) {
+            for (int i = 0; i < sentence.Length + 1; i++) {
+                conversation.text = sentence.Substring(0, i);
+                yield return new WaitForSeconds(TextShowDelay);
+            }
+            yield return new WaitUntil(() => nextEvent);
+        }
+        nextEvent = false;
+        yield return new WaitUntil(() => nextEvent); // wait for one final button click
+        
+        endConvoEvent();
+        GoOutOfFrame();
+    }
+
+    IEnumerator WaitingForNext() {
+        yield return new WaitUntil(() => nextEvent);
+        nextEvent = false;
+    }
+
+    void GoNextLine() {
+        nextEvent = true;
+    }
+
+    void GoOutOfFrame() {
+        Vector3 distance = new Vector3(0, 15, 0);
+        transform.position -= distance;
+        conversation.transform.position -= new Vector3(0, 200, 0);
+        GetComponentInChildren<DownButtonHandler>().transform.position -= distance;
+    }
+
+    void GoInFrame() {
+        Vector3 distance = new Vector3(0, 15, 0);
+        transform.position += distance;
+        conversation.transform.position += new Vector3(0, 200, 0);
+        GetComponentInChildren<DownButtonHandler>().transform.position += distance;
+    }
+} 
