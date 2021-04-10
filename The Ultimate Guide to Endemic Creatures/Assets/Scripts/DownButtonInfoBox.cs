7f4b7b83edad45e681f7dc894920824ca625a5db
iggyvilla <iggyvilla@icloud.com>
2021-04-10 18:04:49 +0800

    Added InfoBox

---
+++ The Ultimate Guide to Endemic Creatures/Assets/Scripts/DownButtonInfoBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownButtonInfoBox : MonoBehaviour
+{
+    public delegate void NextLineDelegate();
+    public event NextLineDelegate continueToQuestionEvent;
+    public AnimationCurve myCurve;
+    bool move = true;
+
+    void Start()
+    {
+        // GetComponentInParent<InfoBoxHandler>().animationDone += startMoving; 
+    }
+
+    void OnMouseDown() {
+        Debug.Log("Down button pressed");
+        continueToQuestionEvent();
+        transform.localScale -= new Vector3(0.05f, 0.05f, 0);
+        move = false;
+    }
+
+    void OnMouseUp()
+    {
+        transform.localScale += new Vector3(0.05f, 0.05f, 0);
+    }
+
+    void startMoving() {
+        move = true;
+    }
+
+}
---
+++ The Ultimate Guide to Endemic Creatures/Assets/Scripts/InfoBoxHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBoxHandler : MonoBehaviour
+{
+    public delegate void AnimationDoneDelegate();
+    public event AnimationDoneDelegate animationDone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<DownButtonInfoBox>().continueToQuestionEvent += exitInfoBox;
+        MakeInfoBoxEvent();
+    }
+
+    public void MakeInfoBoxEvent() {
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("inframe", true);
+    }
+
+    void exitInfoBox() {
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("inframe", false);
+    }
+}
